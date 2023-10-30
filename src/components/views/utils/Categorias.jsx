7646bcb8a75d6b95bc35fcb414a9a0d0837931b6
sofiabmm <42014464+sofiabmm@users.noreply.github.com>
2023-10-30 18:40:27 -0300

    Modificaciòn de detalles, Categorìas y header

--- src/components/views/utils/Categorias.jsx
+++ src/components/views/utils/Categorias.jsx
@@ -1,5 +1,85 @@
 import React from 'react';
 import styled from "styled-components"
+<<<<<<< Updated upstream
+=======
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+
+
+const StyledCategorias = styled.div `
+
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: center;  
+
+  .categorias{
+    display: flex;
+    justify-content: space-between;
+    width: 1330px;
+  }
+
+  .div-h2{
+    display: flex;
+    justify-content: flex-start;
+  }
+
+  .div-categorias{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  .title-categoria{
+    background: #0000002b;
+    font-weight: 700;
+    height: 100%;
+    border-radius: 5px;
+  }
+
+  .css-dasnyc-MuiImageListItemBar-title {
+    font-size: 40px;
+    line-height: normal !important;
+  }
+  
+  .image-categoria{
+    font-size: 40px;
+    font-weight: 700;
+  }
+
+  img.foto{
+    border-radius: 5px;
+    box-shadow: #80808047 2px 2px 2px 1px;
+  }
+
+  li{
+    width: 300px;
+    height: 150px !important;
+  }
+
+  @media (max-width: 1340px) {
+  
+    .categorias{
+      display: flex;
+      justify-content: space-between;
+      width: 1260px;
+    }
+  }
+  @media (max-width: 600px) {
+    .categorias{
+      display: flex;
+      justify-content: space-between;
+      flex-direction: column;
+      align-items: center;
+    }
+    
+
+    li{
+      width: 50vw;
+      
+    }
+}
+>>>>>>> Stashed changes
 
 const StyledCategorias = styled.div`
     
@@ -9,7 +89,10 @@ const Categorias = () => {
   const categorias = ['Destinos', 'Categoria 2', 'Categoria 3'];
 
   return (
+  
+    <>
     <StyledCategorias>
+<<<<<<< Updated upstream
       <h2>Categorías</h2>
       <ul>
         {categorias.map((categoria, index) => (
@@ -21,3 +104,54 @@ const Categorias = () => {
 }
 
 export default Categorias;
+=======
+      <div className='div-categorias'>
+        <div className='div-h2'>
+
+        <h2>Categorías</h2>       
+        </div>
+        <div className='categorias'>
+          { Categoria.map((item) => (
+            <ImageListItem key={item.img}>
+              <img className='foto'
+                srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                loading="lazy"
+              />
+              <ImageListItemBar className='title-categoria' title={item.title}/>
+            </ImageListItem>
+          ))}
+          </div>
+      </div>
+    </StyledCategorias></>
+  );
+}
+
+const Categoria = [
+  {
+    img: 'https://i.pinimg.com/736x/74/e5/55/74e5557fce67fffa50e3eaf128a550e8.jpg',
+    title: 'Norte',
+    
+  },
+  {
+    img: 'https://cdn.pixabay.com/photo/2015/05/28/22/54/argentina-patagonia-788744_1280.jpg',
+    title: 'Sur',
+  
+  }, 
+  {
+    img: 'https://www.guiadecabanias.com/imgs/galerias/postal_1030.jpg',
+    title: 'Este',
+
+  },
+  {
+    img: 'https://cdn.discordapp.com/attachments/1162206131888869456/1167551418647982151/a8GQ9c_x_1256x620__2.webp?ex=654e89fe&is=653c14fe&hm=ef549287ff4b50f3ff80b9d8b12212f4dd2347e5d000bdf144c3f1f985fcf3aa&',
+    title: 'Oeste',
+   
+  }
+  
+];
+
+export default Categorias;
+
+
+>>>>>>> Stashed changes
--- src/components/views/utils/Recomendaciones.jsx
+++ src/components/views/utils/Recomendaciones.jsx
@@ -1,15 +1,74 @@
 import React from 'react';
+import styled from "styled-components"
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-const Recomendaciones = () => {
+const StyledRecomendaciones = styled.div `
+
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    justify-content: center;
+
+.div-h2{
+    display: flex;
+    justify-content: flex-start;
+  }
+.div-recomendaciones{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+}
+
+.recomendaciones{
+    display: flex;
+    justify-content: space-between;
+    width: 1330px;
+}
+`
 
 
+function Recomendaciones () {
+  return (
+    <>
+    <StyledRecomendaciones>
+        <div className='div-recomendaciones'>
 
-    return (
-        <div>
-            <h2>Recomendación</h2>
+        <div className='div-h2'>
+            <h2>Recomendaciones</h2>
             
         </div>
-    );
+        <div className='recomendaciones'>
+
+            <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+                sx={{ height: 140 }}
+                image="/static/images/cards/contemplative-reptile.jpg"
+                title="green iguana"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                Lizard
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over 6,000
+                species, ranging across all continents except Antarctica
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+            </Card>
+        </div>
+        </div>
+    </StyledRecomendaciones>
+    </>
+  );
 }
 
 export default Recomendaciones;
