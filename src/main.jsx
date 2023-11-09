d70792d7ef19442ad00ccf7050fa1dc7f7f7182f
sofiabmm <42014464+sofiabmm@users.noreply.github.com>
2023-11-09 20:01:14 -0300

    agrego inicio de sesion y registro

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Admin from './components/views/admin/Admin.jsx'
 import Registro from './components/views/login/Registro.jsx'
+import Login from './components/views/login/Login.jsx'
 import Inicio from './components/views/Inicio.jsx' 
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 
@@ -14,6 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path='/inicio' element={<Inicio/>} />
         <Route path='/admin' element={<Admin/>}/>
         <Route path='/registro' element={<Registro/>}/>
+        <Route path='/login' element={<Login/>}/>
       </Route>
     </Routes>
   </BrowserRouter>
