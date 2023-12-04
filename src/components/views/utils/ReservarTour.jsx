7fdf0111ba8e224e40d15f7bf3a5e09ad155ddcb
romanpristupa <romanalexis_16_@hotmail.com>
2023-12-04 01:14:41 -0300

    Corregido la fecha

--- src/components/views/utils/ReservarTour.jsx
+++ src/components/views/utils/ReservarTour.jsx
@@ -190,8 +190,8 @@ const ReservaTour = () => {
         }
       };
       const formatDate = (date) => {
-        const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
-        return new Date(date).toLocaleDateString('en-US', options);
+        const isoString = new Date(date).toISOString();
+        return isoString.split('T')[0];
       };
 
       const handleDateChange = (dates) => {
