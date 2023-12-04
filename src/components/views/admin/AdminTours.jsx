04699242b3051cb9057ab8d927a23ac87505ef92
ginofrancocba <ginofrancocba@gmail.com>
2023-12-04 17:33:31 -0300

    Agrego Cambios AdminTours

--- src/components/views/admin/AdminTours.jsx
+++ src/components/views/admin/AdminTours.jsx
@@ -1,32 +1,78 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components';
 import CardTourAdmin from './CardTourAdmin';
-import BasicModal from './Basicmodal';
+import BasicModal from './BasicModal';
+import fetchWithToken from '../login/Interceptor'
+import { useNavigate } from 'react-router-dom';
+import decodeToken from '../login/DecodeToken';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
-import './EstilosAdmin.css';
 
+const StyledAdministracion = styled.div`
+    div.header-table{
+        background-color: #f5f5f5;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        padding: 1rem;
+        margin: 1rem;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        display: flex;
+        justify-content: space-around;
+    }
 
-
-const StyledAdministracion = styled.div` 
-
+    div.pagination-container {
+        display: flex;
+        justify-content: center;
+        margin-top: 20px;  
+    }
 `
 
 const AdminTours = () => {
     const [tours, setTours] = useState([]);
+
+    const navigate = useNavigate();
+    let decodedData = null
+    
+       
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            navigate('/login'); 
+        } else {
+            try {
+                decodedData = decodeToken(localStorage.getItem('token'));
+                console.log(decodedData.role)   
+                getTours();
+
+            } catch (error) {
+                console.error('Error al decodificar el token:', error.message);
+            }
+        }
+    }, [navigate]);
+
+    const getTours = async () => {
+        try {
+            const response = await fetchWithToken("http://localhost:8080/tours/todos");
+         } catch (error) {
+                console.error("Error al obtener los datos de la API: ", error);
+            }
     const [currentPage, setCurrentPage] = useState(1);
 
     const handlePageChange = (event, newPage) => {
         setCurrentPage(newPage);
-    };
+    
+    } 
 
-    const toursPerPage = 10;
+
+    const toursPerPage = 5;
 const startIndex = (currentPage - 1) * toursPerPage;
 const endIndex = startIndex + toursPerPage;
 
     const getTours = async () => {
         try {
             const response = await fetch("http://localhost:8080/tours/todos");
+
             const jsonData = await response.json();
 
             setTours(jsonData);
@@ -45,7 +91,7 @@ const endIndex = startIndex + toursPerPage;
 
     return (
         <StyledAdministracion>
-            <h1 className='adm-tours'>Administración Tours</h1>
+            <h1>Administración Tours</h1>
             <BasicModal onTourAdded={refreshTours}/>
             <div className='header-table'>
                 <span className='id'>ID</span>
@@ -76,7 +122,7 @@ const endIndex = startIndex + toursPerPage;
             </Stack>
             </div>
         </StyledAdministracion>
-    );
+    )}
 };
 
 export default AdminTours;
