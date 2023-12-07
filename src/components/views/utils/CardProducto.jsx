461be370496179f8433643abadf4fc8e5b3b3460
gianlucarobotti200 <gianlucarobotti200@gmail.com>
2023-12-07 03:17:33 -0300

    Cambios estilos en card productos recomendaciones y resultados

--- src/components/views/utils/CardProducto.jsx
+++ src/components/views/utils/CardProducto.jsx
@@ -3,21 +3,16 @@ import styled from "styled-components";
 
 const CardProducto = ({ imgUrl, provincia, titulo, descripcion, precio, duracion }) => {
 
-    
+
     const StyledProduct = styled.article`
         
         border.style: solid 1px black;
     `
 
     return (
-        <StyledProduct>
-            <img src={imgUrl} alt="" />
-            <h3>{titulo}</h3>
-            <h5>Provincia de {provincia}</h5>
-            <p>{descripcion}</p>
-            <span>desde $ {precio}</span>
-            <span>{duracion}</span>
-        </StyledProduct>
+        <StyledLink to={`/detalles/${id}`}>
+            
+        </StyledLink>
     )
 }
 
